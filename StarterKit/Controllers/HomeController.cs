d9222c073f3818f1dee2a5933e0def04159cb6c0
Charlie <1021342@hr.nl>
2024-09-25 15:40:36 +0200

    poging tot inlogpagina

--- StarterKit/Controllers/HomeController.cs
+++ StarterKit/Controllers/HomeController.cs
@@ -25,4 +25,9 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
 }
---
+++ StarterKit/Models/Login.cs
@@ -0,0 +1,7 @@
+namespace StarterKit.Models;
+
+public class Login
+{
+    public int UserName { get; set; }
+    public decimal PassWord { get; set; }
+}
