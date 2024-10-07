2fe8bd0306146387055d6ec862d17d8d92825965
Charlie <1021342@hr.nl>
2024-10-07 15:54:12 +0200

    week 1 af :)

--- StarterKit/Controllers/LoginController.cs
+++ StarterKit/Controllers/LoginController.cs
@@ -4,7 +4,7 @@ using StarterKit.Models;
 using StarterKit.Services;
 
 namespace StarterKit.Controllers;
-
+// https://stackoverflow.com/questions/54868207/how-to-create-and-access-session-net-core-api
 
 [Route("api/v1/Login")]
 public class LoginController : Controller
@@ -17,27 +17,34 @@ public class LoginController : Controller
         _loginService = loginService;
     }
 
-    [HttpPost("Login")] // to do: sessions aanmaken, sessions verwijderen bij uitloggen, 
-    // account meegeven als er ingelogd is, isloggedin check maken
+    [HttpPost("Login")]
     public IActionResult Login([FromBody] LoginBody loginBody)
-    { // EncryptionHelper.EncryptPassword(loginBody.Password)) --> als het encrypted moet worden om te checken
+    {
         LoginStatus status =  _loginService.CheckPassword(loginBody.Username, loginBody.Password);
         if (status == LoginStatus.IncorrectUsername){return Unauthorized("Incorrect username");}
         else if (status == LoginStatus.IncorrectPassword){return Unauthorized("Incorrect password");}
-        else if (status == LoginStatus.Success){return Ok($"Log in succesful for {loginBody.Username}");}
+
+        else if (status == LoginStatus.Success)
+        {
+            HttpContext.Session.SetString("Username", loginBody.Username);
+            return Ok($"Log in succesful for {loginBody.Username}");
+        }
         else{return Unauthorized("Unknown error");}
     }
 
     [HttpGet("IsAdminLoggedIn")]
     public IActionResult IsAdminLoggedIn()
     {
-        // TODO: This method should return a status 200 OK when logged in, else 403, unauthorized
-        return Unauthorized("You are not logged in");
+        var username = HttpContext.Session.GetString("Username");
+
+        if (string.IsNullOrEmpty(username)){return Ok(new {IsLoggedIn = false, Adminusername = (string) null});}
+        return Ok(new {IsLoggedIn = true, AdminUsername = username});        
     }
 
     [HttpGet("Logout")]
     public IActionResult Logout()
     {
+        HttpContext.Session.Clear();
         return Ok("Logged out");
     }
 
